505837ee1a23ae89a93ff22ccaf618a7a93b01c6
Jimmy Ng <jimmy_ng@outlook.de>
2023-06-29 13:07:13 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>BLA</h1>
+  <HeaderTemplate />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import HeaderTemplate from "@/components/HeaderTemplate.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    HeaderTemplate,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
